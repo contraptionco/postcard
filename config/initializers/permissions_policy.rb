09c2ed7bb1722d096fc205b37a37c1ecb9f011c9
Philip I. Thomas <mail@philipithomas.com>
2026-09-05 13:23:48 -0600

    Apply a tested permissions policy without blocking author embeds

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,36 +1,11 @@
 # frozen_string_literal: true
 
-# Define an application-wide HTTP permissions policy. For further
-# information see https://developers.google.com/web/updates/2018/06/feature-policy
+# Rails' permissions_policy DSL still emits the older Feature-Policy header.
+# Use the current Permissions-Policy syntax for browser enforcement.
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Permissions-Policy
 #
-# This policy restricts access to browser APIs that this application does not use,
-# reducing the attack surface and improving security.
-
-Rails.application.config.permissions_policy do |f|
-  # Disable access to sensors
-  f.accelerometer      :none
-  f.gyroscope          :none
-  f.magnetometer       :none
-  f.ambient_light_sensor :none
-
-  # Disable access to media devices
-  f.camera             :none
-  f.microphone         :none
-
-  # Disable access to location
-  f.geolocation        :none
-
-  # Disable access to hardware
-  f.usb                :none
-  f.midi               :none
-
-  # Disable payment and identity APIs
-  f.payment            :none
-
-  # Disable autoplay and picture-in-picture
-  f.autoplay           :none
-  f.picture_in_picture :none
-
-  # Allow fullscreen only from same origin (for viewing postcards)
-  f.fullscreen         :self
-end
+# Postcard does not use device sensors, media capture, location or hardware APIs.
+# Keep media playback, fullscreen and payment at browser defaults so authors can
+# explicitly delegate them to embeds through their iframe allow attributes.
+Rails.application.config.action_dispatch.default_headers['Permissions-Policy'] =
+  'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), usb=()'
