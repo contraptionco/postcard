dc21e7366c2bcfd2f2e2e664faf695f25da3b215
Philip I. Thomas <mail@philipithomas.com>
2026-09-05 13:21:31 -0600

    Include the Ghost setting in schema loads and use dashboard button styles

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_06_000100) do
     t.bigint "ahoy_signup_visit_id"
     t.bigint "pinned_post_id"
     t.datetime "locked_at"
+    t.boolean "sync_to_ghost", default: false, null: false
     t.boolean "grandfathered", default: false, null: false
     t.index ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
     t.index ["email"], name: "index_accounts_on_email", unique: true
